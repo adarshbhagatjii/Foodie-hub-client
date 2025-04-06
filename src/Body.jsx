4bd1a58b85476df248ec9a6dfeb7d5cea33d4073
adarshbhagatjii <bhagatadarsh01@gmail.com>
2025-04-07 00:26:45 +0530

    final updates

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './component/Navbar'
 import { Outlet } from 'react-router'
-import Footer from './component/footer'
+import Footer from './component/Footer'
 import { ToastContainer } from 'react-toastify';
 
 
